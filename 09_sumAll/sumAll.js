ad0082fc495a2148365e7a19ab1d7345c64c61c6
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-06-11 21:30:33 +0300

    exercise 9 done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (
+        typeof a !== 'number' || 
+        typeof b !== 'number' || 
+        !Number.isInteger(a) || 
+        !Number.isInteger(b) || 
+        a < 0 || b < 0
+    ) {
+        return "ERROR";
+    }
+    if(a > b) {
+        let tmp = a;
+        a = b;
+        b = tmp;
+    }
+    let res = 0;
+    for (let i = a; i < b + 1; i++) {
+        res = res + i;
+    }
+    return res;
 };
 
 // Do not edit below this line
