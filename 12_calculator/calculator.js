5b6ffac615a5b702b14a2e85c78bc8bcad001621
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-06-11 21:49:08 +0300

    exercise 12 done

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let res = 0;
+  for (let i = 0; i < a.length; i++) {
+    res += a[i];
+  }
+  return res;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  let res = 1;
+  for (let i = 0; i < a.length; i++) {
+    res = res * a[i];
+  }
+  return res;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let res = 1;
+  for (let i = 0; i < b; i++) {
+    res = res * a;
+  }
+  return res;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1;
+  }
+  res = 1;
+  for (let i = 1; i <= a; i++) {
+    res = res * i;
+  }
+  return res;
 };
 
 // Do not edit below this line
