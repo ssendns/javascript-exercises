b673482baf1fd1be569b70b9bcda510b84fc2590
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-06-11 21:01:35 +0300

    exercise 7 done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split("")
+    arr = arr.reverse();
+    res = arr.join("");
+    return res;
 };
 
 // Do not edit below this line
