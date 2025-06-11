cfa0fd331616c5c8251335f4af7ad48c153974ea
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-06-11 20:56:32 +0300

    exercise 6 done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
+    const arr = [];
+    for(let i = 0; i < num; i++) {
+        arr[i] = str;
+    }
+    res = arr.join("");
+    return res;
 };
 
 // Do not edit below this line
