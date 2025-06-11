cfe14cae9e2efc6610cfaec13ea62cfc2824c00b
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-06-11 21:23:18 +0300

    exercise 8 done

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(arr, ...values) {
+    return arr.filter(item => !values.includes(item));
+  };
+  
 // Do not edit below this line
 module.exports = removeFromArray;
