270217728b879e21e25167a265ea7e048a77706d
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-06-24 11:34:35 +0300

    exercise 13 done

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (s) {
+    let tmp = s.toLowerCase();
+    tmp = tmp.replace(/[^a-z0-9]/g, "");
+    return tmp === tmp.split("").reverse().join("");
 };
 
 // Do not edit below this line
