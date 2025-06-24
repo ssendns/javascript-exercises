7c8c7f315f5960f9e8986674efd296d3ea7c9ee8
Anna Parilova <187332467+ssendns@users.noreply.github.com>
2025-06-24 11:39:22 +0300

    exercise 14 done

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    if (a < 0) return "OOPS";
+    a = Number(a);
 
+    let arr = [];
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (let i = 2; i <= a; i++) {
+        arr[i] = arr[i - 1] + arr[i - 2];
+    }
+
+    return arr[a];
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
